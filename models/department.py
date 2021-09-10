7d06d79fecb0cda1eb0badda48f94ebfb6afafbe
niazi <niazi.mahrab@northsouth.edu>
2021-09-10 11:01:30 +0600

    inherit

--- models/department.py
+++ models/department.py
@@ -1,8 +1,11 @@
 from odoo import models, fields, api
 
-class HospitalDepartment(models.Model):
-    _name = 'kmhospital.department'
-    _description = 'Hospital Department'
+# class HospitalDepartment(models.Model):
+#     _name = 'kmhospital.department'
+#     _description = 'Hospital Department'
 
-    name = fields.Char(string='Department Name', required=True)
-    description = fields.Char(string='Department details')
+#     name = fields.Char(string='Department Name', required=True)
+#     description = fields.Char(string='Department details')
+
+class HospitalDepartment(models.Model):
+        _inherit = "hr.department"
