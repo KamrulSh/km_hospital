05346441318b5dfdf4fed15fd3009544a5a09639
Kamrul Islam <shahin.cuetcse@gmail.com>
2021-09-13 10:37:53 +0600

    security added

--- __manifest__.py
+++ __manifest__.py
@@ -24,7 +24,7 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',    
+        'security/ir.model.access.csv',    
         'views/patient_view.xml',
         'views/doctor_view.xml',
         'views/department_view.xml',
