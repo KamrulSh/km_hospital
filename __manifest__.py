267c719721bb00a9140919c2f6087364bdedf726
Kamrul Islam <shahin.cuetcse@gmail.com>
2021-09-20 10:10:03 +0600

    update manifest file

--- __manifest__.py
+++ __manifest__.py
@@ -1,24 +1,20 @@
 # -*- coding: utf-8 -*-
 {
     'name': "KM Hospital",
-
+    'sequence': 1,
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        A hospital management module.""",
 
     'description': """
-        Long description of module's purpose
+        A hospital management module for creating patient, doctor, 
+        department, medical test, and appointments.
     """,
 
-    'author': "My Company",
+    'author': "Kamrul",
     'website': "http://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Uncategorized',
+    'category': 'Services',
     'version': '0.1',
-
+    'license': 'LGPL-3',
     # any module necessary for this one to work correctly
     'depends': ['base','hr'],
 
@@ -35,8 +31,7 @@
         'reports/sale_report_inherit.xml',
         'reports/appointment_report.xml',
     ],
-    # only loaded in demonstration mode
-    # 'demo': [
-    #     'demo/demo.xml',
-    # ],
+    'installable' : True,
+    'application' : True,
+    'auto_install' : False,
 }
